91675082b3871367f8c18eb42b2a3f86586b66b9
Erik Bakstad <erik.bakstad@gmail.com>
2015-06-29 11:06:27 +0200

    Updated java-client

--- src/main/java/ClientExample.java
+++ src/main/java/ClientExample.java
@@ -1,15 +1,11 @@
 import com.ardoq.ArdoqClient;
-import com.ardoq.model.*;
-import com.ardoq.util.SyncUtil;
-import com.google.common.collect.Lists;
-
-import java.util.Map;
 
 public class ClientExample {
 
     public static void main(String[] args) {
-        ArdoqClient client = new ArdoqClient("http://192.168.59.103", "603c58741e9d4193b4d4ddf3a41a69e0")
-                .setOrganization("statoil");
+        ArdoqClient client = new ArdoqClient("http://192.168.59.103", "676615202ef64142becd775d42eec8a2")
+                .setOrganization("ardoq");
+        
 
     }
 }
